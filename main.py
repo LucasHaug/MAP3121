899aed645d6a3f9d732d40136643827c539fa690
LucasHaug <lucas.haug@hotmail.com>
2020-05-19 19:28:45 -0300

    Update where to save files

--- main.py
+++ main.py
@@ -16,10 +16,18 @@ def main():
     except OSError:
         pass
 
-    # Input parameters
+    # Task selection
     task_number = input("Qual tarefa deseja executar 1 ou 2? ")
     task_letter = input("Qual letra deseja executar a, b ou c? ")
 
+    task_result_dir = f"{results_dir}/{task_number}/{task_letter}"
+
+    try:
+        os.mkdir(task_result_dir)
+    except OSError:
+        pass
+
+    # Input parameters
     N = int(input("Insira o valor de N: "))
     λ = float(input("Insira o valor de λ: "))
 
@@ -34,9 +42,9 @@ def main():
     print(f"N: {N}, M: {M}, λ: {λ}, Δx: {Δx} e Δt: {Δt}")
     
     if task_number == "1":
-        task_one.run(task_letter.lower(), N, M, λ, Δx, Δt, results_dir)
+        task_one.run(task_letter.lower(), N, M, λ, Δx, Δt, task_result_dir)
     else: 
-        task_two.run(task_letter.lower(), N, M, λ, Δx, Δt, results_dir)
+        task_two.run(task_letter.lower(), N, M, λ, Δx, Δt, task_result_dir)
 
     print("Execução finalizada!")
 
