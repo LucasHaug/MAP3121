e6c89475a818b8d65e4aa4a22e9099f308d5daf9
LucasHaug <lucas.haug@hotmail.com>
2020-05-19 15:46:40 -0300

    Add  task 1b

--- task_one.py
+++ task_one.py
@@ -8,7 +8,7 @@ def f_function(t, x, N, letter):
     if letter == "a":
         result = 10 * np.cos(10 * t) * (x**2) * ((1 - x)**2) - (1 + np.sin(10 * t)) * (12 * (x**2) - 12 * x + 2)
     elif letter == "b":
-        pass
+        result = 5 * np.exp(t - x) * ((5 * (t**2) * np.cos(5 * t * x)) - (np.sin(5 * t * x) * (2 * t + x))) 
     elif letter == "c":
         r = 10000 * (1 - 2 * (t**2))
 
@@ -30,7 +30,7 @@ def u_solution(t, x, letter):
     if letter == "a":
         result = (1 + np.sin(10 * t)) * (x**2) * ((1 - x)**2)
     elif letter == "b":
-        pass
+        result = np.exp(t - x) * np.cos(5 * t * x)
     elif letter == "c":
         result = 0
     else:
@@ -42,7 +42,8 @@ def initial_condition(x, letter):
     if letter == "a":
         result = (x**2) * ((1 - x)**2)
     elif letter == "b":
-        pass
+        # result = u_solution(0, x, letter)
+        result = np.exp(-x)
     elif letter == "c":
         result = 0
     else:
@@ -57,8 +58,10 @@ def boundary_conditions(t, letter):
         cond_zero = 0
         cond_one = 0
     elif letter == "b":
-        cond_zero = 0
-        cond_one = 0
+        # cond_zero = u_solution(t, 0, letter)
+        cond_zero = np.exp(t) 
+        # cond_one = u_solution(t, 1, letter)
+        cond_one = np.exp(t - 1) * np.cos(5 * t)
     elif letter == "c":
         cond_zero = 0
         cond_one = 0
