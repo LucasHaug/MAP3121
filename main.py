d1902aa0412019845a8bc59358eea68a5276416c
LucasHaug <lucas.haug@hotmail.com>
2020-05-19 23:15:41 -0300

    Update main

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def main():
     task_number = input("Qual tarefa deseja executar 1 ou 2? ")
     task_letter = input("Qual letra deseja executar a, b ou c? ")
 
+    task_letter = task_letter.lower()
+
     task_result_dir = f"{results_dir}/{task_number}/{task_letter}"
 
     try:
@@ -28,9 +30,9 @@ def main():
         pass
 
     if task_number == "1":
-        task_one.run(task_letter.lower(), task_result_dir)
+        task_one.run(task_letter, task_result_dir)
     else:
-        task_two.run(task_letter.lower(), task_result_dir)
+        task_two.run(task_letter, task_result_dir)
 
     print("Execução finalizada!")
 
