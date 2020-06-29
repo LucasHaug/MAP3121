f7eb180ec18690a1b09ba0905925c942e2bbf15e
Lucas Haug <lucas.haug@hotmail.com>
2020-06-29 00:50:44 -0300

    Add main

---
+++ EP2/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#################################################
+### Main Function
+#################################################
+
+def main():
+    # Test selection
+    # test_letter = input("Qual teste deseja executar a, b, c ou d? ")
+
+    # test_letter = test_letter.lower()
+
+    print("Execução finalizada!")
+
+
+if __name__ == "__main__":
+    main()
