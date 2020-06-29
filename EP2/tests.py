e233b27691700a235ba1156eb55b2f15a617e1ef
Lucas Haug <lucas.haug@hotmail.com>
2020-06-29 12:40:29 -0300

    Add empty tests file

---
+++ EP2/tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import crank_nicolson
+
+#################################################
+### Functions Definitions
+#################################################
+
+def get_data(test_letter):
+    return 0, 0, 0, 0
