e75b1f5a0882eaaa1052f3f3471c3158cccce53c
LucasHaug <lucas.haug@hotmail.com>
2020-07-17 18:59:05 -0300

    Add trapezoidal rule

--- P2/Integração Numérica/numeric_integral.py
+++ P2/Integração Numérica/numeric_integral.py
@@ -12,7 +12,31 @@ def f_function(x):
     Função f a ser integrada
     """
 
-    result = (np.sin(x)) / x
+    result = (np.exp(x))
+
+    return result
+
+
+
+def f_derivative(x, derivative_order):
+    """
+    Derivada n da função f.
+
+    Necesária somente para os cálculos dos erros.
+    """
+
+    if (derivative_order == 1):
+        result = 0
+    elif (derivative_order == 2):
+        result = np.exp(x)
+    elif (derivative_order == 3):
+        result = 0
+    elif (derivative_order == 4):
+        result = 0
+    elif (derivative_order == 5):
+        result = 0
+    else:
+        result = 0
 
     return result
 
@@ -23,11 +47,16 @@ def f_function(x):
 
 
 def main():
-    num_of_points = 3
     integral_max = 1
     integral_min = 0
+    step = 0.25
+    # num_of_points = int((integral_max - integral_min) / step)
+    num_of_points = 10
 
     gaussian_quadrature(num_of_points, integral_max, integral_min)
+
+    trapezoidal_rule(num_of_points, integral_max, integral_min)
+    trapezoidal_rule_error(num_of_points, integral_max, integral_min)
 
 
 #################################################
@@ -52,6 +81,48 @@ def gaussian_quadrature(num_of_points, integral_max, integral_min):
     result *= ((integral_max - integral_min) / 2)
 
     print(result)
+
+
+
+def trapezoidal_rule(num_of_points, integral_max, integral_min):
+    """
+    Regra das trapézios
+    """
+
+    step = (integral_max - integral_min) / num_of_points
+
+    result = f_function(integral_min)
+
+    for i in range(1, num_of_points):
+        result += (2 * f_function(integral_min + i * step))
+
+    result += f_function(integral_max)
+
+    result *= (step / 2)
+
+    print(result)
+
+
+
+def trapezoidal_rule_error(num_of_points, integral_max, integral_min):
+    """
+    Cálculo do erro para a regra das trapézios
+    """
+
+    step = (integral_max - integral_min) / num_of_points
+
+    max_f_second_deriv = abs(f_derivative(integral_min, 2))
+
+    for i in range(1, num_of_points + 1):
+        f_second_deriv_value = abs(f_derivative(integral_min + i * step, 2))
+
+        if (f_second_deriv_value > max_f_second_deriv):
+            max_f_second_deriv = f_second_deriv_value
+
+    error = num_of_points * ((step**3) / 12) * max_f_second_deriv
+
+    print(f"Erro = {error}")
+
 
 #################################################
 ### Main Function Call
