aad8812cff4524752496a03a9e77bb09824e814e
LucasHaug <lucas.haug@hotmail.com>
2020-07-23 21:10:29 -0300

    :heavy_plus_sign: Add dependency for mmq

--- P1/mmq.py
+++ P1/mmq.py
@@ -4,12 +4,14 @@ import numpy as np
 import sympy as sym
 import decimal
 
+import linear_system as linsys
+
 #################################################
 ### Context control
 #################################################
 
 # Number of significant digits
-PRECSION = 4
+PRECSION = 2
 
 # Round to nearest with ties going away from zero.
 # See https://docs.python.org/3/library/decimal.html#rounding-modes
@@ -40,7 +42,7 @@ def main():
 
     print(f"\nMatriz G: \n{g_matrix} \n\nMatriz F: \n{f_array}")
 
-    x_array = solve_linear_system(g_matrix, f_array)
+    x_array = linsys.solve_linear_system(g_matrix, f_array, PRECSION)
 
     print(f"\nResposta X: \n{x_array}")
 
@@ -97,124 +99,6 @@ def generate_linear_system(f_array, g_matrix):
                 a_matrix[j][i] = inner_product
 
     return a_matrix, b_array
-
-
-
-def matrix_decomposition(a_matrix):
-    """
-    Decompõe uma matrix A simétrica em três matrizes L, D e
-    Lt, retornando as duas matrizes L e D.
-
-    Aviso: A matriz A é mudada dentro da função.
-    """
-
-    decimal.getcontext().prec = PRECSION
-    decimal.getcontext().rounding = ROUNDING_MODE
-
-    matrix_dimension = len(a_matrix)
-
-    l_matrix = np.full((matrix_dimension, matrix_dimension), decimal.Decimal('0'))
-    d_matrix = np.full((matrix_dimension, matrix_dimension), decimal.Decimal('0'))
-
-    for f in range(0, matrix_dimension):
-        # Generate L matrix
-        for l in range(f, matrix_dimension):
-            l_matrix[l][f] = a_matrix[l][f] / a_matrix[f][f]
-
-        # Generate the new matrix A
-        for c in range(0, matrix_dimension):
-            for l in range(f + 1, matrix_dimension):
-                a_matrix[l][c] = a_matrix[l][c] - l_matrix[l][f] * a_matrix[f][c]
-
-    # Generate D matrix
-    for l in range(0, matrix_dimension):
-        d_matrix[l][l] = a_matrix[l][l]
-
-    return l_matrix, d_matrix
-
-
-
-def lu_decomposition(a_matrix):
-    """
-    Decompõe uma matrix A simétrica em três matrizes L e
-    U, retornando as duas matrizes
-
-    Aviso: A matriz A é mudada dentro da função.
-    """
-
-    decimal.getcontext().prec = PRECSION
-    decimal.getcontext().rounding = ROUNDING_MODE
-
-    matrix_dimension = len(a_matrix)
-
-    l_matrix = np.full((matrix_dimension, matrix_dimension), decimal.Decimal('0'))
-    u_matrix = np.full((matrix_dimension, matrix_dimension), decimal.Decimal('0'))
-
-    for f in range(0, matrix_dimension):
-        # Generate L matrix
-        for l in range(f, matrix_dimension):
-            l_matrix[l][f] = a_matrix[l][f] / a_matrix[f][f]
-
-        # Generate the new matrix A
-        for c in range(0, matrix_dimension):
-            for l in range(f + 1, matrix_dimension):
-                a_matrix[l][c] = a_matrix[l][c] - l_matrix[l][f] * a_matrix[f][c]
-
-    # Generate U matrix
-    u_matrix = a_matrix
-
-    return l_matrix, u_matrix
-
-
-
-def solve_linear_system(a_matrix, b_array):
-    """
-    Soluciona um sistema Ax = b, onde A é uma matrix simétrica.
-
-    Para a resolução do sistema, é feita a decomposição de A para L*D*Lt.
-
-    É feita a divisão do problema em três sistemas menores:
-    L * y = b
-    D * z = y
-    Lt * x = z
-    """
-
-    decimal.getcontext().prec = PRECSION
-    decimal.getcontext().rounding = ROUNDING_MODE
-
-    matrix_dimension = len(a_matrix)
-
-    l_matrix, d_matrix = matrix_decomposition(a_matrix)
-
-    # First system solution -> L * y = b
-    y_array = np.full(matrix_dimension, decimal.Decimal('0'))
-
-    y_array[0] = b_array[0]
-
-    for l in range(1, matrix_dimension):
-        y_array[l] = b_array[l]
-
-        for m in range(0, l):
-            y_array[l] = y_array[l] - y_array[m] * l_matrix[l][m]
-
-    # Second system solution -> D * z = y
-    z_array = np.full(matrix_dimension, decimal.Decimal('0'))
-
-    for l in range(0, matrix_dimension):
-        z_array[l] = y_array[l] / d_matrix[l][l]
-
-    # Third system solution -> Lt * x = z
-    x_array = np.full(matrix_dimension, decimal.Decimal('0'))
-
-    x_array[-1] = z_array[-1]
-
-    for l in range(matrix_dimension - 2, -1, -1):
-        x_array[l] = z_array[l]
-
-        for m in range(matrix_dimension - 1, l, -1):
-            x_array[l] = x_array[l] - x_array[m] * np.transpose(l_matrix)[l][m]
-
-    return x_array
 
 
 
